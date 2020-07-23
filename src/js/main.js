a5245ae6f97de9f756f6b0a3dd84d232f02d6f02
Stanabol <Aspectdevice1990@gmail.com>
2020-07-23 22:31:14 +0300

    save

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+// Polyfill imports
+import 'core-js/features/object/assign';
+import 'core-js/features/promise';
+
+// Library imports
+import svg4everybody from 'svg4everybody';
+
+
+// Utility imports
+import bxPanelFix from './util/bx-panel-fix';
+import * as FocusUtility from './util/focus';
+import * as LazyLoader from './util/lazy-loader';
+
+
+// Block imports
+// TODO: add code here
+
+// Initialize libraries
+svg4everybody();
+
+
+// Initialize utilities
+FocusUtility.init();
+LazyLoader.init();
+
+bxPanelFix((panel, isFixed) => {
+    void (panel);
+    void (isFixed);
+});
